12994d40ae6cabfa31e5afea2a3afd2541c3a735
akhilpamwani <83491855+akhilpamwani@users.noreply.github.com>
2022-09-17 11:22:53 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+
+import { Hero } from '../Components/Hero/Hero'
+//import { Nav } from '../Components/Navbar/Nav'
+// import { Navbar } from '../Components/Navbar/NavBar'
+
+export default function Home() {
+  return (
+    <>
+      <div className='lg:text-white lg:bg-slate-900 md:text-white md:bg-slate-900 sm:text-white sm:bg-slate-900'>
+{/* <Nav/> */}
+     {/* <Navbar/>     */}
+   <Hero/> 
+      </div>
+      
+    </>
+  )
+}
